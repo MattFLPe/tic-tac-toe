ae43fcaad4c7bceef84e0ff94a5e76cb9126186e
Mateus Felipe Araújo <brasil.mateus748@gmail.com>
2024-01-31 13:31:02 -0300

    Attach event listener to Start Button The game will start, and the board will be displayed
    Attach event listener to the cells
    Cells switch between "X" and "O" on each click

--- script.js
+++ script.js
@@ -1,25 +1,68 @@
 
 const gameBoard = (() => {
     let board = ['', '', '', '', '', '', '', '', '']
+
     const displayBoard = () => {
         let boardHTML = "";
-        gameBoard.forEach((cell, index) => {
+        board.forEach((cell, index) => {
             boardHTML += `<div class="cell" id=cell-${index}">${cell}</div>`
+        });
+        document.querySelector("#board").innerHTML = boardHTML;
+        const cells = document.querySelectorAll(".cell")
+        let currentPlayer = "X"
+        cells.forEach(cell => {
+            cell.addEventListener("click", () => {
+                if(!cell.textContent){
+                    cell.textContent = currentPlayer
+                    currentPlayer = currentPlayer === "X" ? "O" : "X"
+                } else {
+                    alert("Cell already filled!")
+                }
+            })
         })
-    }
-    document.querySelector("#board").innerHTML = boardHTML;
+
+    };
+    return { displayBoard };
 })();
 
 const Player = (name, symbol) => {
     return {name, symbol};
 };
 
-const displayController = (() => {
+const Game = (() => {
+    let players = [];
+    let currentPlayer;
+    let gameOver;
     
+    const startGame = () => {
+        players = [
+            Player(document.querySelector("#player1").value, "X"),
+            Player(document.querySelector("#player2").value, "o")
+        ];
+
+        currentPlayer = 0;
+        gameOver = false;
+        gameBoard.displayBoard();
+    };
+     return { startGame }
 })();
 
-const Game = (() => {
+const startBtn = document.querySelector("#start-btn");
+startBtn.addEventListener("click", () => {
+    Game.startGame();
+});
 
+const restartBtn = document.querySelector("#restart-btn");
+startBtn.addEventListener("click", () => {
+    gameBoard.displayBoard();
+});
+
+
+
+
+/*
+const checkWinner = () => {
+    const board = gameBoard.displayBoard();
     const winConditions = [
         [0, 1, 2],
         [3, 4, 5],
@@ -30,10 +73,5 @@ const Game = (() => {
         [0, 4, 8],
         [2, 4, 6]
     ];
-    function checkWinner() {
-
-    }
-})();
-
-
-
+};
+*/
